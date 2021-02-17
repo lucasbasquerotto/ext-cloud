c0d88e5ebb1e51ad6104dcf49dd8524f2dc26137
Lucas Basquerotto <lucasbasquerotto@gmail.com>
2021-02-16 23:37:56 -0300

    [dev] test - collections

---
+++ test/collections/my_org_02/my_collection_02_02/plugins/filter/main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+# pylint: disable=missing-module-docstring
+# pylint: disable=missing-class-docstring
+# pylint: disable=missing-function-docstring
+
+from __future__ import absolute_import, division, print_function
+
+
+class FilterModule(object):
+  def filters(self):
+    return {
+        'filter_01': self.filter_01,
+        'filter_02': self.filter_02,
+    }
+
+  def filter_01(self, value):
+    return 'org2-collection2-filter1' + ' -> ' + str(value)
+
+  def filter_02(self, value):
+    return 'org2-collection2-filter2' + ' -> ' + str(value)
