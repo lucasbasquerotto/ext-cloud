47696421a884f7c32512bde13fc141abcf9476bf
Lucas Basquerotto <lucasbasquerotto@gmail.com>
2021-05-22 11:46:59 -0300

    [dev] mailcow

--- files/mailcow/run-backup.tpl.sh
+++ files/mailcow/run-backup.tpl.sh
@@ -7,7 +7,7 @@ dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # environment vars
 
-BACKUP_LOCATION={{ params.backup_dir | default('/opt/backup') | quote }}
+export BACKUP_LOCATION={{ params.backup_dir | default('/opt/backup') | quote }}
 
 # general vars
 s3_alias={{ params.s3_alias | quote }}
@@ -28,17 +28,17 @@ endpoint={{ params.endpoint | default('') | quote }}
 volume="/root/.aws/config"
 command=( aws --profile="$s3_alias" s3 )
 [ -n "$endpoint" ] && command+=( --endpoint="$endpoint" )
-command+=( sync "$BACKUP_LOCATION" "s3://$bucket_name/$bucket_path" )
+command+=( sync '/var/backup' "s3://$bucket_name/$bucket_path" )
 
 {% elif params.s3_cli == 'mc' %}
 
 volume="/root/.mc/config.json"
-command=( mirror --overwrite "$BACKUP_LOCATION" "$s3_alias/$bucket_name/$bucket_path" )
+command=( mirror --overwrite '/var/backup' "$s3_alias/$bucket_name/$bucket_path" )
 
 {% elif params.s3_cli == 'rclone' %}
 
 volume="/config/rclone/rclone.conf"
-command=( copy --verbose "$BACKUP_LOCATION" "$s3_alias:$bucket_name/$bucket_path" )
+command=( copy --verbose '/var/backup' "$s3_alias:$bucket_name/$bucket_path" )
 
 {% else %}
 
@@ -49,8 +49,15 @@ command=( copy --verbose "$BACKUP_LOCATION" "$s3_alias:$bucket_name/$bucket_path
 
 # execute local backup
 
+if [ ! -d "$BACKUP_LOCATION" ]; then
+  mkdir "$BACKUP_LOCATION"
+fi
+
 {{ input.pod_dir }}/helper-scripts/backup_and_restore.sh backup all
 
 # execute remote backup (s3)
 
-docker run --rm -v "$dir/config.s3.txt":"$volume" "$container_image" "${command[@]}"
+docker run --rm \
+  -v "$dir/config.s3.txt":"$volume" \
+  -v "$BACKUP_LOCATION":/var/backup \
+  "$container_image" "${command[@]}"
