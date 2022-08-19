4a554c2f2b62cd950970318f39991512e2d7a530
Lucas Basquerotto <lucasbasquerotto@gmail.com>
2022-08-19 18:07:30 -0300

    Refactor code

--- collection/plugins/module_utils/nameserver/godaddy_nameserver.py
+++ collection/plugins/module_utils/nameserver/godaddy_nameserver.py
@@ -15,8 +15,8 @@
 from __future__ import absolute_import, division, print_function
 __metaclass__ = type  # pylint: disable=invalid-name
 
-import requests
 import traceback
+import requests
 
 from ansible_collections.lrd.cloud.plugins.module_utils.lrd_utils import ordered
 from ansible_collections.lrd.ext_cloud.plugins.module_utils.vars import prepare_default_data
